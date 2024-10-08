5434af3a7e1beb8853b43936d14c5c238432da35
Andy <asteel542@gmail.com>
2024-10-08 13:31:23 +1100

    Add files via upload

---
+++ Compare-File-Script.ps1
@@ -0,0 +1,5 @@
+﻿$fileA = "C:\Scripts"
+$fileB = "C:\Scripts"
+if (Compare-Object -ReferenceObject $(Get-Content $fileA) -DifferenceObject $(Get-Content $fileB))
+    {"files are different"}
+Else {"Files are the same"}
